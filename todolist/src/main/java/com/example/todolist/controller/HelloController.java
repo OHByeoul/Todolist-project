897aa2d8c3638dbbb5886e89658d492665ca6cda
OHByeoul <suriessay@naver.com>
2017-08-15 14:19:40 +0900

    1. 데이터베이스 dependency 삽입 2. application.properties에 데이터베이스 연동 3. 어플리케이션 작동 확인 위해 테스트 코드 작성

---
+++ todolist/src/main/java/com/example/todolist/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/todolist", method = RequestMethod.GET)
+public class HelloController {
+	@RequestMapping(value = "/hello")
+	@ResponseBody
+	public String hello(){
+		return "running test";
+	}
+}
